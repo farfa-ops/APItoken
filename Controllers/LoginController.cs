96b93196c533de436f892cbccf9865c6346013e7
TMY <TMY@DESKTOP-SK2CQKL>
2025-02-21 11:05:52 +0800

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using APItoken.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace APItoken.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel login) //API token
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJSONWebToken(user); //generate token
+                response = Ok(new { token = tokenString }); //return token
+            }
+            return response;
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if (login.Username == "johndoe")
+            {
+                user = new UserModel
+                {
+                    Username = login.Username,
+                    Password = login.Password
+                };
+            }
+            return user;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256); // security and performance must be balnace
+            var claims = new[] //tell engine that token is belong to who
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.Username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())  //mac address
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"],
+              claims, expires: DateTime.Now.AddSeconds(120), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using APItoken.Models;
+using Microsoft.EntityFrameworkCore;
+namespace APItoken.Controllers { 
+    [ApiController]
+    //[Authorize]
+    [Route("api/[controller]/[action]")]
+
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Id")]
+        public async Task<ActionResult>GetById(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        return product == null ? NotFound() : Ok(product);
+        
+        }
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
+
+
